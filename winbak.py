3e4cc06be77f49cc7db9a259113ac9a54f025742
Tov Are <tovare13@gmail.com>
2021-11-05 16:06:03 +0100

    Pushing for the Weekend.

--- winbak.py
+++ winbak.py
@@ -1,16 +1,64 @@
 import shutil
 import pathlib
 import os
+import re
+import sys
+from getpass import getpass, getuser
+import socket
+from tkinter import Tk, filedialog
 
+import fnmatch
 
+def func_smb_get_server_address():
+    smb_server_address = input("SMB IP or Hostname: ")
+    if re.search(r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$', smb_server_address):
+        print("IP: " + smb_server_address)
+        pass
+    if re.search(r'^[A-Za-z]+$', smb_server_address):
+        try: 
+            smb_server_address = socket.gethostbyname(smb_server_address)
+            print("Hostname: " + smb_server_address)
+            return(smb_server_address)
 
+        except Exception as e: 
+            error = str(e)
+            if (error.find("[Errno 11001]") != -1):
+                print("Error getting IP address. Check spelling of hostname.")
+                sys.exit
+            else: 
+                print("Unknown Error: " + str(error))
+                sys.exit
+    else: 
+        print("Error with Server address. Check speilling.")
+        sys.exit
 
 source_dir: str = pathlib.Path('C:/WORKFOLDER/WinBak/Test Folders/Source/')
 target_dir: str = pathlib.Path('C:/WORKFOLDER/WinBak/Test Folders/Destination/')
 
 list_of_files = []
 
-source_drive_letter = "C"
+# source_drive_letter = input("Source Drive Letter: ")
+print("Choose witch drive to backup: ")
+source_drive_letter = pathlib.Path(filedialog.askdirectory(title = "Choose drive to backup"))
+print(source_drive_letter)
+
+#Setup TKinter:
+root = Tk() #Sett root to Tk()
+root.withdraw() #Hides small tkinger window. Try without it once.
+root.attributes('-topmost', True) #Open tkinter window over everything else.
+
+smb_domain = input("SMB Domain: ")
+smb_user = input("SMB Username: ") 
+smb_password = getpass(prompt="SMB Password: ")
+smb_local_machine_name = socket.gethostname()
+# smb_server_name = input("SMB Server name: ")
+# smb_server_address = func_smb_get_server_address()
+# smb_server_backup_location = 
+print("Select backup target location: ")
+smb_server_backup_target_location = pathlib.Path(filedialog.askdirectory(title = "Select backup target location")) #returns Server location Path
+print(smb_server_backup_target_location)
+
+#TODO: The above script needs to handle empty inputs.
 
 excluded_files_and_folders = [
 ":/WORKFOLDER/WinBak/Test Folders/Source/Folder2",
@@ -22,7 +70,7 @@ excluded_files_and_folders = [
 working_excluded_files_and_folders = []
 for items in excluded_files_and_folders:
     
-    working_excluded_files_and_folders.append(pathlib.Path(source_drive_letter + items))
+    working_excluded_files_and_folders.append(pathlib.Path(str(source_drive_letter) + items))
 
 #Walk down Source_dir to find folder and files
 for root, dirs, files in os.walk(source_dir, topdown=False):
@@ -33,7 +81,8 @@ for root, dirs, files in os.walk(source_dir, topdown=False):
         x = (pathlib.Path(os.path.join(root, name)))  
         list_of_files.append(x)
     for item in list_of_files:
-        if item in working_excluded_files_and_folders:
+        # if item in working_excluded_files_and_folders:
+        if (fnmatch.filter(item, working_excluded_files_and_folders)): #Does not expect list
             print("Found exception. removing")
             list_of_files.remove(item)
         else: 
