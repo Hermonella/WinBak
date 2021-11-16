b8340ef1e1ecd43018ef56435ee31b273b232396
Tov Are <tovare13@gmail.com>
2021-11-16 13:06:29 +0100

    setup download from git + cleanup

--- setup.py
+++ setup.py
@@ -1,23 +1,40 @@
 import os
 import platform
 
+
+
+
 dep = [
     "hurry.filesize==0.9",
     "tqdm==4.62.3",
+    "GitPython"
 ]
 
 
 #Install dependencies:
 for i in dep:
-    os.system("pip install " + i)
+    os.system("pip3 install " + i)
 
 if platform.system() == "Windows":
-    os.system("pip install wmi")
-    os.system("pip install tkinter")
+    os.system("pip3 install wmi")
+    os.system("pip3 install tk")
 if platform.system() == 'Linux':
-    os.system("pip install psutil")
+    os.system("pip3 install psutil")
     print("\nInstalling tkinter via apt-get. For some reason this can't be done via pip...")
-    os.system("sudo apt-get install python3-tk -y")
+    os.system("sudo apt install python3-tk -y")
+    print("\nSame for git.")
+    os.system("sudo apt install git -y")
     
 
-os.system("python winbak.py")
+print("\n\n\n\n\n\n")
+#Download WinBak from Github. SMB is too hard at this stage og the script.
+from git import Repo
+Repo.clone_from(
+    "https://github.com/Hermonella/WinBak.git",
+     "/tmp/WinBak",
+     branch='Copy-From-Linux-Live-USB')
+
+input("Press Enter")
+
+
+# os.system("python winbak.py")
